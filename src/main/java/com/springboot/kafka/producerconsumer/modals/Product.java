8f38f57a54724c59a4061594f344611e8092d52d
Kannan Paramasivam <kannan.paramasivam@grainger.com>
2022-11-13 16:09:25 -0600

    product modal added

---
+++ src/main/java/com/springboot/kafka/producerconsumer/modals/Product.java
@@ -0,0 +1,14 @@
+package com.springboot.kafka.producerconsumer.modals;
+
+public class Product {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+}
